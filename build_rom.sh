e55389cbcd3644b41bfe225f7657cf1ee2184e84
ninihao9 <118429604+ninihao9@users.noreply.github.com>
2023-05-30 12:11:25 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,13 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/IdkAnythin07/local_manifest.git --depth 1 -b Pixel_exp .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
+aosp_tissot-userdebug
 m aex -j$(nproc --all)
 
 # upload rom
