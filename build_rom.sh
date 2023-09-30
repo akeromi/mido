7fa3e490f1c38695855aec99d73656b4c2be62ca
ninihao9 <118429604+ninihao9@users.noreply.github.com>
2023-09-30 13:39:22 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,13 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IdkAnythin07/local_manifest.git --depth 1 -b Pixel_exp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ninihao9/lol.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-aosp_tissot-userdebug
+lunch lineage_star2lte-userdebug
 m aex -j$(nproc --all)
 
 # upload rom
